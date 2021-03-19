c19423b335d998ab1979ae0ad072184267d6ab18
kingrz01 <67392353+kingrz01@users.noreply.github.com>
2021-03-19 13:55:48 +0100

    Update url_shortener.py
    characters moved in the global part, to generate them just once.

--- url_shortener.py
+++ url_shortener.py
@@ -1,18 +1,17 @@
 repo = {} #repository for urls and new shorten ids
 domain = 'kratki.com' # domain of the new url after shortening
-id = 100000000 # new ids will be given by a counter. The start number is big to have at least 5 charcters after encoding
+id = 1 # new ids will be given by a counter. The number will be encoded in 62decimal code. Try starting id with big number
+letters_low = [chr(char) for char in range(ord('a'),ord('z')+1)] #all letter lower
+letters_up = [chr(char) for char in range(ord('A'),ord('Z')+1)] #all letter upper
+numbers = [str(num) for num in range(10)] #all numbers
+alphanum = numbers+letters_low+letters_up #all alfanumeric characters
+base = len(alphanum) # base 62 for encoding
 
 def shorten(url):
     '''insert full link to get a kratki.com short link'''
     global id, repo
     
     def encode(id): #encoder from base 10 to base 62
-        
-        letters_low = [chr(char) for char in range(ord('a'),ord('z')+1)] #all letter lower
-        letters_up = [chr(char) for char in range(ord('A'),ord('Z')+1)] #all letter upper
-        numbers = [str(num) for num in range(10)] #all numbers
-        alphanum = numbers+letters_low+letters_up #all alfanumeric characters
-        base = len(alphanum) # base 62
         result = [] #coding output list
         while id > 0: #formula for encoding from base 10 to base x; ex. for HEX numbers on base 16
             position = id % base #index of the character, for the alphanum list
@@ -28,3 +27,5 @@ def shorten(url):
         id += 1 #id counter grow by 1
     
     return f'{domain}/{short}' #new short link with new domain
+print(shorten('google.com'))
+print(shorten('yahoo.com'))
